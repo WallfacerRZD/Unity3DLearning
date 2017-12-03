64a51ee3d5586e160f90ece032a522b5558df1a3
WallfacerRZD <1048058400@qq.com>
2017-12-03 13:50:51 +0800

    projects initialize

---
+++ test/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public GameObject BulletPrefab;
+    public float speed = 3;
+	// Use this for initialization
+	void Start () {
+       
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = GameObject.Instantiate(BulletPrefab, this.transform.position, this.transform.rotation);
+            Rigidbody rgb = bullet.GetComponent<Rigidbody>();
+            rgb.velocity = transform.forward * speed;
+        }
+    }
+}
