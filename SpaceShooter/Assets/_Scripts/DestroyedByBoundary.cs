b49a16c75e78a1dda0c102411e2d6391893c4ba3
WallfacerRZD <1048058400@qq.com>
2017-12-11 23:18:23 +0800

    inite

---
+++ SpaceShooter/Assets/_Scripts/DestroyedByBoundary.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedByBoundary : MonoBehaviour {
+
+    // Update is called once per frame
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+        Debug.Log("Trigger!");
+    }
+}
---
+++ StickPin/Assets/Scripts/Pin.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public float speed = 5;
+    private bool isFly = false;
+    private bool isReach = false;
+    private Transform startPoint;
+    private Transform circle;
+    private Vector3 targetCirclePos;
+    // Use this for initialization
+    void Start()
+    {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.Find("Circle").transform;
+        targetCirclePos = circle.position;
+        targetCirclePos.y -= 2.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFly == false)
+        {
+            if (isReach == false)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)
+                {
+                    isReach = true;
+                }
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetCirclePos, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, targetCirclePos) < 0.05f)
+            {
+                isFly = false;
+                transform.position = targetCirclePos;
+                transform.parent = circle;
+            }
+        }
+    }
+
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+
+    }
+
+    
+}
