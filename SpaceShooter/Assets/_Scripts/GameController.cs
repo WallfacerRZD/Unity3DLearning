7c885476446db677e66174e5286514a9c1f9656e
WallfacerRZD <1048058400@qq.com>
2017-12-12 21:04:45 +0800

    GameController

--- SpaceShooter/Assets/_Scripts/GameController.cs
+++ SpaceShooter/Assets/_Scripts/GameController.cs
@@ -6,21 +6,33 @@ public class GameController : MonoBehaviour {
 
     public GameObject asteriod;
     public Vector3 spawnValue;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveTime;
 
 	// Use this for initialization
 	void Start () {
-        Spawn();
+        StartCoroutine(SpawnWaves());
 	}
 
-    void Spawn()
+    IEnumerator SpawnWaves()
     {
-        Vector3 spawnPositon = new Vector3(
-            Random.Range(-spawnValue.x, spawnValue.x),
-            spawnValue.y,
-            spawnValue.z);
-
-        Quaternion spwnRotation = Quaternion.identity;
-        Instantiate(asteriod, spawnPositon, spwnRotation);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; ++i)
+            {
+                Vector3 spawnPositon = new Vector3(
+                    Random.Range(-spawnValue.x, spawnValue.x),
+                    spawnValue.y,
+                    spawnValue.z);
+                Quaternion spwnRotation = Quaternion.identity;
+                Instantiate(asteriod, spawnPositon, spwnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveTime);
+        }
     }
 	
 }
