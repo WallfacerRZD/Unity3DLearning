8307b9263e2f85e8dbf3cea915811a246439ea02
WallfacerRZD <1048058400@qq.com>
2017-12-12 17:42:23 +0800

    spawn

---
+++ SpaceShooter/Assets/_Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject asteriod;
+    public Vector3 spawnValue;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+	}
+
+    void Spawn()
+    {
+        Vector3 spawnPositon = new Vector3(
+            Random.Range(-spawnValue.x, spawnValue.x),
+            spawnValue.y,
+            spawnValue.z);
+
+        Quaternion spwnRotation = Quaternion.identity;
+        Instantiate(asteriod, spawnPositon, spwnRotation);
+    }
+	
+}
